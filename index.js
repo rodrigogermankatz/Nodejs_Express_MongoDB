5ee14f117b2e4e7c84c62243069f53d06c6531b7
rodrigo <rodrigog.katz@hotmail.com>
2022-03-24 19:09:43 -0300

    Execute using node index.js require the FileSystem module

---
+++ index.js
@@ -0,0 +1,13 @@
+const fileSystem = require('fs');
+
+const hello = "hello world";
+console.log(hello);
+
+
+/**
+ * DOCUMENTATION:
+ * https://nodejs.org/en/docs/
+ * 
+ * Execute using node index.js
+ * 
+ * **/ 
