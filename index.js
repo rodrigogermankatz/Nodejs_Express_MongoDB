ae20b9c3346b8e9e706bd16685df07a27651d6ad
rodrigo <rodrigog.katz@hotmail.com>
2022-03-24 19:35:51 -0300

    reading and writing files sychronous way

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const fileSystem = require('fs');
 const hello = "hello world";
 console.log(hello);
 
+//sychronous version of file reading
+const inputText = fileSystem.readFileSync('./txt/input.txt', 'utf-8'); //if i don't pass the second param it will be type buffer
+console.log(inputText);
+
+//sychronous version of file writing
+const textOutput = `el inputText --> ${inputText} <-- fue creado el ${Date.now()}`;
+fileSystem.writeFileSync('./txt/output.txt',textOutput);
+console.log(`File Written!`);
 
 /**
  * DOCUMENTATION:
